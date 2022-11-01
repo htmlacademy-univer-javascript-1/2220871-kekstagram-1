53c8da60eac3d0695970d53f37b0a9696a8658ea
Anna <anastasia0sky@gmail.com>
2022-11-01 16:05:07 +0500

    Task4

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
+firstPartDescript = [];
+secondPartDescript = [];
 
-function getRandom (min, max) {
-  if(min >= 0 && max <= min){
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-  else{
-    return false;
-  }
+message = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+
+];
+numberOfPhoto = 25;
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
-getRandom(0, 1000);
 
-
-function stringType (userString, maxLen){
-  if(userString.length > maxLen){
-    return false;
-  }
-  else{
-    return true;
-  }
+function checkStringLength (string, length) {
+  return string.length <= maxLength;
 }
-stringType('Дичь!', 1000);
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+
+
+
+
+
+//for (let i; i<=25; i++){}
+
+let myObject = {
+id : getRandomPositiveInteger(1,25),
+url : 'photos/' + getRandomPositiveInteger(1,25) + '.jpg',
+description : 'Новое фото',
+likes : getRandomPositiveInteger(15,200),
+comments :
+
+};
